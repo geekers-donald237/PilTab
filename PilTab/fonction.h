3f24604513f3216fdc8c2457893d4c63f30a11e1
geekers-donald237 <96464111+geekers-donald237@users.noreply.github.com>
2022-07-03 07:40:12 +0100

    Add files via upload

---
+++ PilTab/fonction.h
@@ -0,0 +1,33 @@
+//Made by MBIADA_BAYON_IDRIS_21Q2915
+#ifndef __FONCTION__H__
+#define __FONCTION_H__
+#define MAX 50
+
+
+
+typedef struct pile
+{
+    int tab[MAX];
+    int sommet;
+} PILE;
+
+
+void initialisation(PILE *p);
+
+int estVide(PILE *p);
+
+int estPleine(PILE *P);
+
+void empiler(PILE *P, int c);
+
+int depiler(PILE *p);
+
+int sommet(PILE *p);
+
+void Afficher(PILE *p);
+
+void vider(PILE*p);
+
+
+
+#endif
